d079cf3bec1e3bdb3007c24106c0f21f83f3dd0c
qkrwjdan <madogisa12@naver.com>
2020-01-03 23:18:59 +0900

    큰 틀잡기
    메뉴 페이지 만들어 놨수다
    
    to do
    -my 구독 페이지 상세 디테일 만들기
    -모델을 만들어야되는데 무슨모델을 만들어야되는가
    -프로필 모델
    -my구독에서 구독하고 있는 서비스에 대한 모델

--- index/urls.py
+++ index/urls.py
@@ -2,5 +2,13 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('',index,name = 'index')
+    path('',index,name = 'index'),
+    path('mysubs',mysubs,name='mysubs'),
+    path('recommend',recommend,name='service_recommend'),
+    path('honey',honey,name='honey_information'),
+    path('magazine',magazine,name='magazine'),
+    path('event',event,name='event'),
+    path('notice',notice,name='notice'),
+    path('profile',profile,name='profile'),
+    
 ]
--- index/views.py
+++ index/views.py
@@ -4,3 +4,24 @@ from django.shortcuts import render
 
 def index(request):
     return render(request,'index.html')
+
+def mysubs(request):
+    return render(request,'mysubs.html')
+
+def recommend(request):
+    return render(request,'recommend.html')
+
+def honey(request):
+    return render(request,'honey.html')
+
+def magazine(request):
+    return render(request,'magazine.html')
+
+def event(request):
+    return render(request,'event.html')
+
+def notice(request):
+    return render(request,'notice.html')
+
+def profile(request):
+    return render(request,'profile.html')
