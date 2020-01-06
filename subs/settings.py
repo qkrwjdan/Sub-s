38f56a746a1c6db0f024a546dbd28767b5862c37
1996yyk <1996yyk@gmail.com>
2020-01-06 19:20:50 +0900

    css 추가, 반영 실패
    css를 추가했는데 적용이안됨

--- subs/settings.py
+++ subs/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -120,6 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"), # Root의 static 파일
+    '/static/',    # garden App의 static 파일
+)
 
 LOGIN_REDIRECT_URL = '/'
-
